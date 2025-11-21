4e3a57ae756e47729c9f2fe0d3f526fac81b9a35
bernacode <christiandelisoure12@gmail.com>
2025-11-20 21:00:16 -0400

    Se agregaron archivos y se escribio codigo

---
+++ src/analisis.py
@@ -0,0 +1,13 @@
+
+def types_asymmetry(skew):
+        if skew > 0:
+              return "Positiva"
+        elif skew < 0:
+              return "Negativa"
+        else:
+              return "Simetrica"
+
+
+def types_kurtosis(kurtosis):
+
+    return
